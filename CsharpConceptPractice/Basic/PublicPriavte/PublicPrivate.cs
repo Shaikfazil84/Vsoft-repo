893e5f1ef61631e357af2f6c36c00a0906c4eaec
ShaikFazil <shaikfazil837@gmail.com>
2026-08-07 10:28:40 +0530

    publicprivate created

---
+++ CsharpConceptPractice/Basic/PublicPriavte/PublicPrivate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student =new Student();
+        student.ShowStudent();
+        Employee employee =new Employee();
+        employee.ShowEmployee();
+        Car car =new Car();
+        car.ShowCar();
+    }
+}
+class Student
+{
+    public void ShowStudent()
+    {
+        Console.WriteLine("student Details");
+        DisplayName();
+    }
+    private void DisplayName()
+    {
+        Console.WriteLine("Name:Fazil");
+    }
+}
+class Employee
+{
+    public void ShowEmployee()
+    {
+        Console.WriteLine("Emplayee details");
+        DispalyName();
+    }
+    private void DispalyName()
+    {
+        Console.WriteLine("Salary:30000");
+    }
+}
+class Car
+{
+    public void ShowCar()
+    {
+        Console.WriteLine("car deatils");
+        DisplayName();
+    }
+    private void DisplayName()
+    {
+        Console.WriteLine("Model:BMW");
+    }
+}
